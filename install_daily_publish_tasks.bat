abf68abc76b46906f25c9e632e3899c432cd9818
sanghyun ahn <dkstkd840425@gmail.com>
2026-04-22 09:34:59 +0900

    Schedule ETF update at 0925

--- install_daily_publish_tasks.bat
+++ install_daily_publish_tasks.bat
@@ -1,16 +1,24 @@
 @echo off
 cd /d "%~dp0"
-echo This will register two Windows Scheduled Tasks:
-echo - ETF KRX Publish 0835 at 08:35
-echo - ETF KRX Publish 0905 at 09:05
+echo This will register one Windows Scheduled Task:
+echo - ETF KRX Publish 0925 at 09:25
 echo.
 
-taskscheduler /? >nul 2>nul
+echo Removing old 08:35 and 09:05 tasks if they exist...
+schtasks /End /TN "ETF KRX Publish 0835" >nul 2>nul
+schtasks /End /TN "ETF KRX Publish 0905" >nul 2>nul
+schtasks /Delete /TN "ETF KRX Publish 0835" /F >nul 2>nul
+schtasks /Delete /TN "ETF KRX Publish 0905" /F >nul 2>nul
 
-schtasks /Create /TN "ETF KRX Publish 0835" /TR "\"%~dp0run_collect_publish.bat\"" /SC DAILY /ST 08:35 /F
-schtasks /Create /TN "ETF KRX Publish 0905" /TR "\"%~dp0run_collect_publish.bat\"" /SC DAILY /ST 09:05 /F
+echo Creating 09:25 task...
+schtasks /Create /TN "ETF KRX Publish 0925" /TR "\"%~dp0run_collect_publish.bat\"" /SC DAILY /ST 09:25 /F
+if errorlevel 1 (
+  echo ERROR: Scheduled task creation failed.
+  pause
+  exit /b 1
+)
 
 echo.
-echo Scheduled tasks were created.
+echo Scheduled task was created at 09:25.
 echo You can close this window.
 pause
